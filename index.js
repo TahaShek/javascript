7fc5a17182f6b5fedc767c14072d928539bc90b4
TahaSekk <sohailtaha128@gmail.com>
2022-07-15 14:02:10 +0500

    commit

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ let InputValue=()=>{
     let DivwhereIwantMymessage = document.querySelector('.showMessage')
     let MyInputField= document.querySelector('#exampleInputText1')
     let MyInputValue = document.querySelector('#exampleInputText1').value;
-    let radiobutton= document.querySelector('#HelloRadio').value
+    let radiobutton= document.querySelector('#HelloRadio:checked').value
+    let checkbox =document.querySelector('.Value1').value
+    //  checkbox =document.querySelector('.Value2').value
+
+    console.log(radiobutton);
+    console.log(checkbox);
     
 if(MyInputValue=== 'taha@gmail.com' )
 {
     MyInputField.style.border='2px solid green';
     DivwhereIwantMymessage.innerHTML='Approved'
-    
-
 }    
 else{
     MyInputField.style.border= '1px solid red'
@@ -18,7 +21,19 @@ else{
 }
 
   } catch (error) {
-    alert(error.message);
+    // alert(error.message);
+    console.log(error);
     
   }
 }
+
+
+let GetSevenValue=()=>{
+    let sevenValue = parseInt(document.querySelector('.seven').dataset.seven);
+    console.log(sevenValue);
+}
+let GetEightValue=()=>{
+    let EightValue = parseInt(document.querySelector('.eight').dataset.eight);
+    console.log(EightValue);
+}
+
